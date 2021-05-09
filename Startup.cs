20021bd954d5c6279b98f7e2a4d0c54a9109257f
Henry <hnr.leto11@gmail.com>
2021-05-09 19:04:02 -0400

    more cleaning + model add

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Blazor.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Blazor
 {
@@ -28,7 +22,7 @@ namespace Blazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
